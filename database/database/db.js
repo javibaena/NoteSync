0c7ad5845f6c1176d6542d3202026d5d86e376b0
JaviBaena <59729766+javibaena@users.noreply.github.com>
2023-07-04 15:59:37 +0200

    2

---
+++ database/database/db.js
@@ -0,0 +1,18 @@
+const mysql = require ("mysql");
+
+ //conexion base de datos
+const conexion = mysql.createConnection({
+
+    host: "sql8.freesqldatabase.com",
+    port:  3306,
+    user:"sql8616872",
+    password:"nFjLCpZhCu",
+    database:"sql8616872"
+  });
+  
+  conexion.connect((err) => {
+    if (err) throw err;
+    console.log('Conectado a la base de datos MySQL');
+  });
+  /* exportamos la conexion a la base de datos */
+  module.exports = conexion; 
